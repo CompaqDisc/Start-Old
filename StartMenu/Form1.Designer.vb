cc62dd6756acef5d688f8db18960994a1b901b90
Herobrine2Nether <18bradan.wolbeck@gmail.com>
2015-07-17 18:57:04 -0600

    Initial Commit

---
+++ StartMenu/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StartMenuMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.UserDirBtn = New System.Windows.Forms.Button()
+        Me.UserDocsBtn = New System.Windows.Forms.Button()
+        Me.UserPicsBtn = New System.Windows.Forms.Button()
+        Me.UserMusicBtn = New System.Windows.Forms.Button()
+        Me.UserVidBtn = New System.Windows.Forms.Button()
+        Me.UserDownloadsBtn = New System.Windows.Forms.Button()
+        Me.Seperator1 = New System.Windows.Forms.Label()
+        Me.Seperator2 = New System.Windows.Forms.Label()
+        Me.TopSeperator = New System.Windows.Forms.Label()
+        Me.UserGamesBtn = New System.Windows.Forms.Button()
+        Me.UserCompBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.AutoSize = True
+        Me.UsernameLabel.BackColor = System.Drawing.Color.Transparent
+        Me.UsernameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UsernameLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UsernameLabel.Location = New System.Drawing.Point(12, 9)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(83, 20)
+        Me.UsernameLabel.TabIndex = 0
+        Me.UsernameLabel.Text = "%USER%"
+        Me.UsernameLabel.UseMnemonic = False
+        '
+        'UserDirBtn
+        '
+        Me.UserDirBtn.BackColor = System.Drawing.Color.Transparent
+        Me.UserDirBtn.FlatAppearance.BorderColor = System.Drawing.Color.Turquoise
+        Me.UserDirBtn.FlatAppearance.BorderSize = 0
+        Me.UserDirBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue
+        Me.UserDirBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UserDirBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UserDirBtn.Location = New System.Drawing.Point(229, 46)
+        Me.UserDirBtn.Name = "UserDirBtn"
+        Me.UserDirBtn.Size = New System.Drawing.Size(125, 35)
+        Me.UserDirBtn.TabIndex = 1
+        Me.UserDirBtn.Text = "%USERDIR%"
+        Me.UserDirBtn.UseVisualStyleBackColor = False
+        '
+        'UserDocsBtn
+        '
+        Me.UserDocsBtn.BackColor = System.Drawing.Color.Transparent
+        Me.UserDocsBtn.FlatAppearance.BorderColor = System.Drawing.Color.Turquoise
+        Me.UserDocsBtn.FlatAppearance.BorderSize = 0
+        Me.UserDocsBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue
+        Me.UserDocsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UserDocsBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UserDocsBtn.Location = New System.Drawing.Point(229, 87)
+        Me.UserDocsBtn.Name = "UserDocsBtn"
+        Me.UserDocsBtn.Size = New System.Drawing.Size(125, 35)
+        Me.UserDocsBtn.TabIndex = 2
+        Me.UserDocsBtn.Text = "Documents"
+        Me.UserDocsBtn.UseVisualStyleBackColor = False
+        '
+        'UserPicsBtn
+        '
+        Me.UserPicsBtn.BackColor = System.Drawing.Color.Transparent
+        Me.UserPicsBtn.FlatAppearance.BorderColor = System.Drawing.Color.Turquoise
+        Me.UserPicsBtn.FlatAppearance.BorderSize = 0
+        Me.UserPicsBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue
+        Me.UserPicsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UserPicsBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UserPicsBtn.Location = New System.Drawing.Point(229, 128)
+        Me.UserPicsBtn.Name = "UserPicsBtn"
+        Me.UserPicsBtn.Size = New System.Drawing.Size(125, 35)
+        Me.UserPicsBtn.TabIndex = 3
+        Me.UserPicsBtn.Text = "Pictures"
+        Me.UserPicsBtn.UseVisualStyleBackColor = False
+        '
+        'UserMusicBtn
+        '
+        Me.UserMusicBtn.BackColor = System.Drawing.Color.Transparent
+        Me.UserMusicBtn.FlatAppearance.BorderColor = System.Drawing.Color.Turquoise
+        Me.UserMusicBtn.FlatAppearance.BorderSize = 0
+        Me.UserMusicBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue
+        Me.UserMusicBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UserMusicBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UserMusicBtn.Location = New System.Drawing.Point(229, 169)
+        Me.UserMusicBtn.Name = "UserMusicBtn"
+        Me.UserMusicBtn.Size = New System.Drawing.Size(125, 35)
+        Me.UserMusicBtn.TabIndex = 4
+        Me.UserMusicBtn.Text = "Music"
+        Me.UserMusicBtn.UseVisualStyleBackColor = False
+        '
+        'UserVidBtn
+        '
+        Me.UserVidBtn.BackColor = System.Drawing.Color.Transparent
+        Me.UserVidBtn.FlatAppearance.BorderColor = System.Drawing.Color.Turquoise
+        Me.UserVidBtn.FlatAppearance.BorderSize = 0
+        Me.UserVidBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue
+        Me.UserVidBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UserVidBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UserVidBtn.Location = New System.Drawing.Point(229, 216)
+        Me.UserVidBtn.Name = "UserVidBtn"
+        Me.UserVidBtn.Size = New System.Drawing.Size(125, 35)
+        Me.UserVidBtn.TabIndex = 5
+        Me.UserVidBtn.Text = "Videos"
+        Me.UserVidBtn.UseVisualStyleBackColor = False
+        '
+        'UserDownloadsBtn
+        '
+        Me.UserDownloadsBtn.BackColor = System.Drawing.Color.Transparent
+        Me.UserDownloadsBtn.FlatAppearance.BorderColor = System.Drawing.Color.Turquoise
+        Me.UserDownloadsBtn.FlatAppearance.BorderSize = 0
+        Me.UserDownloadsBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue
+        Me.UserDownloadsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UserDownloadsBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UserDownloadsBtn.Location = New System.Drawing.Point(229, 257)
+        Me.UserDownloadsBtn.Name = "UserDownloadsBtn"
+        Me.UserDownloadsBtn.Size = New System.Drawing.Size(125, 35)
+        Me.UserDownloadsBtn.TabIndex = 6
+        Me.UserDownloadsBtn.Text = "Downloads"
+        Me.UserDownloadsBtn.UseVisualStyleBackColor = False
+        '
+        'Seperator1
+        '
+        Me.Seperator1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Seperator1.Location = New System.Drawing.Point(226, 295)
+        Me.Seperator1.Name = "Seperator1"
+        Me.Seperator1.Size = New System.Drawing.Size(128, 13)
+        Me.Seperator1.TabIndex = 7
+        Me.Seperator1.Text = "————————————————————"
+        '
+        'Seperator2
+        '
+        Me.Seperator2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Seperator2.Location = New System.Drawing.Point(226, 390)
+        Me.Seperator2.Name = "Seperator2"
+        Me.Seperator2.Size = New System.Drawing.Size(128, 13)
+        Me.Seperator2.TabIndex = 8
+        Me.Seperator2.Text = "————————————————————"
+        '
+        'TopSeperator
+        '
+        Me.TopSeperator.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.TopSeperator.Location = New System.Drawing.Point(12, 29)
+        Me.TopSeperator.Name = "TopSeperator"
+        Me.TopSeperator.Size = New System.Drawing.Size(342, 14)
+        Me.TopSeperator.TabIndex = 10
+        Me.TopSeperator.Text = "———————————————————————————————————————————————————————"
+        '
+        'UserGamesBtn
+        '
+        Me.UserGamesBtn.BackColor = System.Drawing.Color.Transparent
+        Me.UserGamesBtn.FlatAppearance.BorderColor = System.Drawing.Color.Turquoise
+        Me.UserGamesBtn.FlatAppearance.BorderSize = 0
+        Me.UserGamesBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue
+        Me.UserGamesBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UserGamesBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UserGamesBtn.Location = New System.Drawing.Point(229, 311)
+        Me.UserGamesBtn.Name = "UserGamesBtn"
+        Me.UserGamesBtn.Size = New System.Drawing.Size(125, 35)
+        Me.UserGamesBtn.TabIndex = 11
+        Me.UserGamesBtn.Text = "Games"
+        Me.UserGamesBtn.UseVisualStyleBackColor = False
+        '
+        'UserCompBtn
+        '
+        Me.UserCompBtn.BackColor = System.Drawing.Color.Transparent
+        Me.UserCompBtn.FlatAppearance.BorderColor = System.Drawing.Color.Turquoise
+        Me.UserCompBtn.FlatAppearance.BorderSize = 0
+        Me.UserCompBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue
+        Me.UserCompBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UserCompBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UserCompBtn.Location = New System.Drawing.Point(229, 352)
+        Me.UserCompBtn.Name = "UserCompBtn"
+        Me.UserCompBtn.Size = New System.Drawing.Size(125, 35)
+        Me.UserCompBtn.TabIndex = 12
+        Me.UserCompBtn.Text = "Computer"
+        Me.UserCompBtn.UseVisualStyleBackColor = False
+        '
+        'StartMenuMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(366, 650)
+        Me.Controls.Add(Me.UserCompBtn)
+        Me.Controls.Add(Me.UserGamesBtn)
+        Me.Controls.Add(Me.TopSeperator)
+        Me.Controls.Add(Me.Seperator2)
+        Me.Controls.Add(Me.Seperator1)
+        Me.Controls.Add(Me.UserDownloadsBtn)
+        Me.Controls.Add(Me.UserVidBtn)
+        Me.Controls.Add(Me.UserMusicBtn)
+        Me.Controls.Add(Me.UserPicsBtn)
+        Me.Controls.Add(Me.UserDocsBtn)
+        Me.Controls.Add(Me.UserDirBtn)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "StartMenuMain"
+        Me.Opacity = 0.8R
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Start"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents UsernameLabel As System.Windows.Forms.Label
+    Friend WithEvents UserDirBtn As System.Windows.Forms.Button
+    Friend WithEvents UserDocsBtn As System.Windows.Forms.Button
+    Friend WithEvents UserPicsBtn As System.Windows.Forms.Button
+    Friend WithEvents UserMusicBtn As System.Windows.Forms.Button
+    Friend WithEvents UserVidBtn As System.Windows.Forms.Button
+    Friend WithEvents UserDownloadsBtn As System.Windows.Forms.Button
+    Friend WithEvents Seperator1 As System.Windows.Forms.Label
+    Friend WithEvents Seperator2 As System.Windows.Forms.Label
+    Friend WithEvents TopSeperator As System.Windows.Forms.Label
+    Friend WithEvents UserGamesBtn As System.Windows.Forms.Button
+    Friend WithEvents UserCompBtn As System.Windows.Forms.Button
+
+End Class
---
+++ StartMenu/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class StartMenuMain
+
+    Private Sub StartMenuMain_LostFocus(sender As Object, e As EventArgs) Handles MyBase.LostFocus
+        'Me.Close()
+    End Sub
+
+    Private Sub StartMenuMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MyBase.Focus()
+        Dim ScreenSize = Screen.PrimaryScreen.WorkingArea
+        Dim X As Integer = 0
+        Dim Y As Integer = ScreenSize.Height - Me.Size.Height
+        Me.Location = New Point(X, Y)
+        UsernameLabel.Text = My.User.CurrentPrincipal.Identity.Name.ToString
+        Me.BackgroundImage = Image.FromFile(".\Themes\Default\Background.png")
+    End Sub
+End Class
